b7c7ed67802c68aeba89616deb75f7dfa4e1e658
Emily Stacy <beyonce@thinkful.com>
2019-04-09 16:11:24 -0500

    added to github.io

---
+++ index.js
@@ -0,0 +1,23 @@
+/*function scrollDown(){
+  ("#bio").on('click',function(e){
+    e.preventDefault();
+    window.scrollTo();
+  });
+};*/
+
+/*function menuResponsive (){
+ $("#hamburgerMenu").on('click',function(){
+$("#menuItems").show(); 
+});
+console.log('menuResponsive ran');
+};*/
+
+$("#hamburgerMenu").on('click',function(){ 
+    $("#menuItems").toggle('menuResponsive ran'); 
+    }); 
+    
+    $("#hamburgerMenu").focus(function(){ 
+    $("#menuItems").toggle('menuResponsive ran'); 
+    }); 
+    
+    
